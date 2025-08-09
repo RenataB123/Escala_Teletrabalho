20cb23c4b4bfe8c70d89754ecacb9ed3db2ac298
RenataB123 <renatabispo@uni9.edu.br>
2025-08-09 15:14:56 -0300

    feat: Add simple GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Escala_Teletrabalho/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  server: {
-    port: 5173,
-    open: true,
-    host: true,
-  },
+    assetsDir: 'assets'
+  }
 })
